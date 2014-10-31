60b7309a559385465c139cefb5021d5f6d3fa6f2
Cosmin Dordea <cosmin.dordea@yopeso.com>
2014-10-31 08:17:28 +0200

    Moved from original library to separate component

---
+++ tests/Refactoring/Time/Interval/SpecificMonthTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cdordea
+ * Date: 18/10/14
+ * Time: 19:56
+ */
+
+namespace Refactoring\Time\Interval;
+
+
+class SpecificMonthTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    function itHasSpecificMonthStartAndEnd()
+    {
+        $day = new \DateTime();
+        $day->add(new \DateInterval('P27D'));
+
+        $specificMonth = new SpecificMonth($day);
+
+        $this->assertEquals(new \DateTime('first day of next month midnight'),$specificMonth->getStart());
+        $this->assertEquals(new \DateTime('last day of next month 23:59:59'),$specificMonth->getEnd());
+    }
+
+    function itCanInitWithFebruary2014()
+    {
+        $februaryDay = new \DateTime('2014-02-05');
+
+        $specificMonth = new SpecificMonth($februaryDay);
+
+        $this->assertEquals(new \DateTime('2014-02-01 00:00:00'), $specificMonth->getStart());
+        $this->assertEquals(new \DateTime('2014-02-28 23:59:59'), $specificMonth->getEnd());
+    }
+
+}
